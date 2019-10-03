df89521cba8bb703aae79d4e438be40b72af4647
ethanharrison <harrem18@wfu.edu>
2019-10-03 18:07:38 -0400

    Game now loops for one player until win

--- TTTGame.cpp
+++ TTTGame.cpp
@@ -16,29 +16,32 @@ int main() {
     newGame.initializeBoard();
     newGame.printBoard();
 
-    //Prompt for input
-    cout << endl << endl << "Enter Row and Column: " << endl;
-    int chooseRow;
-    int chooseCol;
+    while (!newGame.checkForWin()) {
 
-    //Player enters choices
-    cin >> chooseRow;
-    cin >> chooseCol;
+        //Prompt for input
+        cout << endl << endl << "Enter Row and Column: " << endl;
+        int chooseRow;
+        int chooseCol;
 
-    //Check to make sure input is within range
-    while (chooseRow < 1 || chooseRow > 3 || chooseCol < 1 || chooseCol > 3) {
-        cout << "Error! Invalid entry, guess again" << endl << "Enter Row and Column: " << endl;
+        //Player enters choices
         cin >> chooseRow;
         cin >> chooseCol;
+
+        //Check to make sure input is within range
+        while (chooseRow < 1 || chooseRow > 3 || chooseCol < 1 || chooseCol > 3) {
+            cout << "Error! Invalid entry, guess again" << endl << "Enter Row and Column: " << endl;
+            cin >> chooseRow;
+            cin >> chooseCol;
+        }
+
+        // Scales input so user can enter numbers 1-3
+        chooseRow--;
+        chooseCol--;
+
+        //Update and print game board
+        newGame.placeMark(chooseRow, chooseCol);
+        newGame.printBoard();
+
     }
-
-    // Scales input so user can enter numbers 1-3
-    chooseRow--;
-    chooseCol--;
-
-    //Update and print game board
-    newGame.placeMark(chooseRow, chooseCol);
-    newGame.printBoard();
-
     return 0;
 }
